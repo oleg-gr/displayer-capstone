5cd11ebf062c060da70b84f3324532be1c4f694f
Oleg Grishin <og402@nyu.edu>
2015-03-29 00:06:43 +0400

    Lots of gridster stuff

--- ui_code/staticfiles/js/custom_task/script.js
+++ ui_code/staticfiles/js/custom_task/script.js
@@ -1,8 +1,31 @@
 $(function(){ //DOM Ready
 
-    $(".gridster ul").gridster({
-        widget_margins: [10, 10],
-        widget_base_dimensions: [100, 50]
-    });
+    // gridster to hold time values
+    $("#gridster1 > div").gridster({
+            widget_selector : "div",
+            widget_margins : [3, 3],
+            // each col represents one second, 5 min max for now
+            min_cols: 300,
+            max_cols: 300,
+            // base dimension is 10 seconds
+            widget_base_dimensions : [10, 20],
+            namespace: '#gridster1'
+        }).data('gridster').disable();
+
+    var gridster = $("#gridster2 > div").gridster({
+        widget_selector : "div",
+        widget_margins : [3, 3],
+        // each col represents one second, 5 min max for now
+        min_cols: 300,
+        max_cols: 300,
+        // base dimension is 10 seconds
+        widget_base_dimensions : [10, 40],
+        // allow resize only in x direction
+        resize : {
+            enabled : true,
+            axes : ['x']
+        },
+        namespace: '#gridster2'
+    }).data('gridster');
 
 });
